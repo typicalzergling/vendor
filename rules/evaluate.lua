ee155383895434082654c1495f86e0992e718d2e
typicalzergling <malvir@live.com>
2018-07-10 11:35:38 -0700

    Add support for already collected toys

--- rules/evaluate.lua
+++ rules/evaluate.lua
@@ -2,23 +2,23 @@
 -- TODO: Make this a dynamic system with default rules and allow user-supplied rules.
 function Vendor:EvaluateItemForSelling(item)
 
-	-- Nil is expected for empty bag slots
-	if not item then return false end
+    -- Nil is expected for empty bag slots
+    if not item then return false end
 
-	-- Blacklist first. We will never sell something on this list, regardless of whatever other criteria it meets.
-	if self:IsItemIdInNeverSellList(item.Id) then
-		return false
-	end
+    -- Blacklist first. We will never sell something on this list, regardless of whatever other criteria it meets.
+    if self:IsItemIdInNeverSellList(item.Id) then
+        return false
+    end
 
-	-- Whitelist comes after blacklist. User said to always sell it, so we will always sell it.
-	if self:IsItemIdInAlwaysSellList(item.Id) then
-		return true
-	end
-	
-	-- Items with zero value cannot be sold.
-	if item.UnitValue == 0 then
-		return false
-	end
+    -- Whitelist comes after blacklist. User said to always sell it, so we will always sell it.
+    if self:IsItemIdInAlwaysSellList(item.Id) then
+        return true
+    end
+    
+    -- Items with zero value cannot be sold.
+    if item.UnitValue == 0 then
+        return false
+    end
 
     -- Sell gray garbage.
     if item.Quality == 0 then
@@ -30,37 +30,41 @@ function Vendor:EvaluateItemForSelling(item)
         return true
     end
     
-	-- We don't sell anything epic or higher or white items, which usually have special meaning or crafting materials.
-	if item.Quality > 3 or item.Quality == 1 then 
-		return false
-	end
-	
+    -- Don't sell uncollected transmogs.
+    if item.IsUnknownAppearance then
+        return false
+    end
 
-	
-	-- Don't sell uncollected transmogs.
-	if item.IsUnknownAppearance then
-		return false
-	end
-	
-	
-	-- From here on out we don't sell soulbound.
-	-- TODO: This should be an option, but it is dangerous to allow it.
-	if item.IsSoulbound then
-		return false
-	end
-	
-	-- Auto sell green gear per settings.
-	if self.db.profile.sellgreens and item.Quality == 2 and (item.TypeId == 2 or item.TypeId == 4) and item.Level < self.db.profile.sellgreensilvl then
-		return true
-	end
-	
-	-- Auto sell blue gear per settings.
-	if self.db.profile.sellblues and item.Quality == 3 and (item.TypeId == 2 or item.TypeId == 4) and item.Level < self.db.profile.sellbluesilvl then
-		return true
-	end
+    -- Sell Collected Soulbound toys. Non-soulbound could be sold on AH (crafted, for example).
+    if item.IsToy and item.IsAlreadyKnown and item.IsSoulbound then
+        return true
+    end
+    
+    -- From here on out we don't sell soulbound.
+    -- TODO: This should be an option, but it is dangerous to allow it.
+    if item.IsSoulbound then
+        return false
+    end
 
-	-- Doesn't fit one of our above sell criteria so we keep it.
-	return false
+
+
+-- We don't sell anything epic or higher or white items, which usually have special meaning or crafting materials.
+    if item.Quality > 3 or item.Quality == 1 then 
+        return false
+    end
+    
+    -- Auto sell green gear per settings.
+    if self.db.profile.sellgreens and item.Quality == 2 and (item.TypeId == 2 or item.TypeId == 4) and item.Level < self.db.profile.sellgreensilvl then
+        return true
+    end
+    
+    -- Auto sell blue gear per settings.
+    if self.db.profile.sellblues and item.Quality == 3 and (item.TypeId == 2 or item.TypeId == 4) and item.Level < self.db.profile.sellbluesilvl then
+        return true
+    end
+
+    -- Doesn't fit one of our above sell criteria so we keep it.
+    return false
 end
 
 
--- sys/utils.lua
+++ sys/utils.lua
@@ -127,3 +127,51 @@ function Vendor:GetPriceString(price)
 	return table.concat(str)
 end
 
+--@do-not-package@
+-- Sorted Pairs from Lua-Users.org. We use this for pretty-printing tables for debugging purposes.
+
+function Vendor.__genOrderedIndex( t )
+    local orderedIndex = {}
+    for key in pairs(t) do
+        table.insert( orderedIndex, key )
+    end
+    table.sort( orderedIndex )
+    return orderedIndex
+end
+
+function Vendor.orderedNext(t, state)
+    -- Equivalent of the next function, but returns the keys in the alphabetic
+    -- order. We use a temporary ordered key table that is stored in the
+    -- table being iterated.
+
+    local key = nil
+    --print("orderedNext: state = "..tostring(state) )
+    if state == nil then
+        -- the first time, generate the index
+        t.__orderedIndex = Vendor.__genOrderedIndex( t )
+        key = t.__orderedIndex[1]
+    else
+        -- fetch the next value
+        for i = 1,table.getn(t.__orderedIndex) do
+            if t.__orderedIndex[i] == state then
+                key = t.__orderedIndex[i+1]
+            end
+        end
+    end
+
+    if key then
+        return key, t[key]
+    end
+
+    -- no more value to return, cleanup
+    t.__orderedIndex = nil
+    return
+end
+
+function Vendor.orderedPairs(t)
+    -- Equivalent of the pairs() function on tables. Allows to iterate
+    -- in order
+    return Vendor.orderedNext, t, nil
+end
+
+--@end-do-not-package@
