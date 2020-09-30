1568eda75da63d1c817c07960c81dfe0eb3ae134
Umferth <umferth@outlook.com>
2020-09-29 19:21:10 -0700

    Update init.lua
    Fix errant character in init.lua

--- sys/init.lua
+++ sys/init.lua
@@ -7,6 +7,8 @@
 -- If this already exists we will assert, as that means you either screwed up and didn't load this first,
 -- or it means someone else already defined the namespace, and we're in Undefined Behavior land.
 local AddonName = select(1,...)
+
+print(...);
 if _G[AddonName] or _G[AddonName.."_GET"] or _G[AddonName.."_LOC"] then
     assert(false, "Addon conflict detected. Addon already exists with this name: "..AddonName)
 end
@@ -15,7 +17,7 @@ _G[AddonName]  = {}
 -- Setup our Addon & frame.
 local Addon = _G[AddonName]
 Addon.AddonFrame = CreateFrame("Frame")
-Addon.Is`Classic = (WOW_PROJECT_ID  == WOW_PROJECT_CLASSIC);
+Addon.IsClassic = (WOW_PROJECT_ID  == WOW_PROJECT_CLASSIC);
 
 -- Event Handling
 local events = {}
