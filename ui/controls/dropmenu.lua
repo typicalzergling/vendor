2ca2a92db4ca7ccb8357acf2bb35eee738318303
umferth@outlook.com <umferth@outlook.com>
2022-02-22 14:50:37 -0800

    Fix the background of item list, edit and dropdowns
    Blizzard changed the constants to a color object, we need to call
    GetRGB() first, or use r/g/b directly.  Fix all the places where
    we use background to handle it.
    
    NOTE: this should move to a utility mixin so that we can just invoke
    it all of the places.

--- ui/controls/dropmenu.lua
+++ ui/controls/dropmenu.lua
@@ -7,6 +7,19 @@ local DropMenu = {}
 function DropMenu:OnLoad()
     self.expanded = false
     self:OnBackdropLoaded()
+
+	if (self.backdropBorderColor) then
+		local alpha = self.backdropBorderColorAlpha or 1
+		local color = self.backdropBorderColor or RED_FONT_COLOR
+		self:SetBackdropBorderColor(color.r, color.g, color.b, alpha)
+	end
+
+	if (self.backdropColor) then 
+		local alpha = self.backdropColorAlpha or 1
+		local color = self.backdropColor or RED_FONT_COLOR
+		self:SetBackdropColor(color.r, color.g, color.b, alpha)
+	end
+        
     self:SetScript("OnMouseDown", function(_, button)
         if (not self.expanded) then
             self.expanded = true
--- ui/edit.lua
+++ ui/edit.lua
@@ -71,6 +71,19 @@ end
 
 function EditHost:OnLoad()
 	self:OnBackdropLoaded();
+	
+	if (self.backdropBorderColor) then
+		local alpha = self.backdropBorderColorAlpha or 1
+		local color = self.backdropBorderColor or RED_FONT_COLOR
+		self:SetBackdropBorderColor(color.r, color.g, color.b, alpha)
+	end
+
+	if (self.backdropColor) then 
+		local alpha = self.backdropColorAlpha or 1
+		local color = self.backdropColor or RED_FONT_COLOR
+		self:SetBackdropColor(color.r, color.g, color.b, alpha)
+	end
+
 	if (self.Scroll) then
 		self:AdjustScrollBar();
 		ScrollFrame_OnLoad(self.Scroll);		
